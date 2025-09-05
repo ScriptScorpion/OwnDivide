04fdcb8cde354a07959b588e9a2cbfefb21d3e7a
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-09-05 19:46:56 +0300

    Create divide.h

---
+++ divide.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdlib.h>
+static inline int divide(long int dividend, long int divisor) {
+    if (divisor == 0 || dividend == 0) {
+        return 0;
+    }
+    
+    long int result = 0;
+    long int remainder = abs(dividend); // you can edit function to return remainder
+    long int abs_divisor = abs(divisor); // removing negative values
+    
+    while (remainder >= abs_divisor) {
+        remainder = remainder - abs_divisor;
+        ++result;
+    }
+    
+    if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+        result = -result;
+    }
+    
+    return result;
+}
