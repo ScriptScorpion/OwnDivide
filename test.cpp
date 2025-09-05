c5eaa999472c411d44acbf9c4f52ab28e776e686
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-09-05 19:50:21 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "divide.h"
+int main() {
+    std::cout << divide(10, 5) << std::endl;
+    std::cout << divide(10, -5) << std::endl;
+    std::cout << divide(1, 500) << std::endl;
+    return 0;
+}
