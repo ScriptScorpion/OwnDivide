6a286f986c70bc6813ada854edcd8fb221c53598
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-09-11 13:17:32 +0300

    Update divide.h

--- divide.h
+++ divide.h
@@ -20,3 +20,23 @@ static inline int divide(const long int dividend, const long int divisor) {
     
     return result;
 }
+static inline int remainder(const long int dividend, const long int divisor) {
+    if (divisor == 0 || dividend == 0) {
+        return 0;
+    }
+    
+    long int result = 0;
+    long int remainder = abs(dividend); // you can edit function to return remainder
+    long int abs_divisor = abs(divisor); // removing negative values
+    
+    while (remainder >= abs_divisor) {
+        remainder = remainder - abs_divisor;
+        ++result;
+    }
+    
+    if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+        result = -result;
+    }
+    
+    return remainder;
+}
