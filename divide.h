eff9e13f6be897721ade210af45595d7c8752b82
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2026-04-28 16:52:16 +0300

    Update divide.h

--- divide.h
+++ divide.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <stdlib.h>
-static inline int divide(const long int dividend, const long int divisor) {
+constexpr long int divide(const long int dividend, const long int divisor) noexcept {
     if (divisor == 0 || dividend == 0) {
         return 0;
     }
     
     long int result = 0;
-    long int remainder = abs(dividend); // you can edit function to return remainder
+    long int remainder = abs(dividend);
     long int abs_divisor = abs(divisor); // removing negative values
     
     while (remainder >= abs_divisor) {
@@ -20,13 +20,13 @@ static inline int divide(const long int dividend, const long int divisor) {
     
     return result;
 }
-static inline int remainder(const long int dividend, const long int divisor) {
+constexpr long int remainder(const long int dividend, const long int divisor) noexcept {
     if (divisor == 0 || dividend == 0) {
         return 0;
     }
     
     long int result = 0;
-    long int remainder = abs(dividend); // you can edit function to return remainder
+    long int remainder = abs(dividend);
     long int abs_divisor = abs(divisor); // removing negative values
     
     while (remainder >= abs_divisor) {
